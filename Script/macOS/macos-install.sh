5ea523be926f3ba706301c3aae85972ef1ab1403
ResRipper <16681844+ResRipper@users.noreply.github.com>
2023-08-19 11:15:40 +0800

    Update macos-install.sh
    Remove redundant `--forcequitapps` argument

--- Script/macOS/macos-install.sh
+++ Script/macOS/macos-install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# Notice: startosinstall always cause auto-reboot, you might want to put it in custom attribute and trigger when user sign-out
+
 installer_name="Install macOS Monterey.app"
 
 if [ -e "/Applications/$installer_name" ]; then
-    "/Applications/$installer_name/Contents/Resources/startosinstall" --agreetolicense --forcequitapps >> /tmp/os_upgrade.log 2>&1
+    "/Applications/$installer_name/Contents/Resources/startosinstall" --agreetolicense >> /tmp/os_upgrade.log 2>&1
 else
     exit 0
 fi
