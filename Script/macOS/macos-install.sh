5eb95f1d2bf354168e12f278001b8e1e9bc3f300
ResRipper <resripper@connective.link>
2023-08-22 17:45:30 +0800

    Update macos-install.sh
    - Add description
    - Add free space detection
    - Add error log info

--- Script/macOS/macos-install.sh
+++ Script/macOS/macos-install.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
+# Install macOS system upgrade
 # Notice: startosinstall always cause auto-reboot, you might want to put it in custom attribute and trigger when user sign-out
 
 installer_name="Install macOS Monterey.app"
+space_required=40 # Required free space (GB)
+
+upgrade_log="/tmp/os_upgrade.log"
 
 if [ -e "/Applications/$installer_name" ]; then
-    "/Applications/$installer_name/Contents/Resources/startosinstall" --agreetolicense >> /tmp/os_upgrade.log 2>&1
+    free_space=$(df -g / | awk 'FNR==2 {print $4}')
+    if [ "$free_space" -lt $space_required ]; then
+        echo "[$(date)] Error: space not enough, required: $space_required GB, remain: $free_space GB" >> $upgrade_log 2>&1
+        exit 1
+    else
+        echo "[$(date)] System upgrade started" >> $upgrade_log 2>&1
+        "/Applications/$installer_name/Contents/Resources/startosinstall" --agreetolicense >> $upgrade_log 2>&1
+    fi
 else
-    exit 0
+    echo "[$(date)] Error: Installer not found." >> $upgrade_log 2>&1
+    exit 1
 fi
