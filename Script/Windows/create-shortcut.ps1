065553072010d8d5c40de7c33700f6f467025dec
ResRipper <16681844+ResRipper@users.noreply.github.com>
2023-08-26 14:39:53 +0800

    Create create-shortcut.ps1
    - Find if `.exe` file in path
    - Create shortcut for all users if file exist

---
+++ Script/Windows/create-shortcut.ps1
@@ -0,0 +1,18 @@
+﻿# Create Application shortcut for all users
+#   - Modified from answer on Stack Overflow
+#     https://stackoverflow.com/questions/9701840/how-to-create-a-shortcut-using-powershell
+
+$name = "Application"
+$exe_path = "$env:ProgramFiles\...\Application.exe"
+$icon = "$env:ProgramFiles\...\Application.ico"
+
+if (Test-Path $exe_path) {
+    $WshShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut("$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$name.lnk")
+    $Shortcut.TargetPath = $exe_path
+    $Shortcut.IconLocation = $icon
+    $Shortcut.Save()
+}
+else {
+    exit 1
+}
